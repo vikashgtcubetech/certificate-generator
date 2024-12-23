c030b9a6501319570439f594a466acc36243487b
vikashgtcubetech <vikashsaini6640@gmail.com>
2024-12-23 15:16:03 +0530

    message

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  bash:"/certificate-generator/",
+   base: '/certificate-generator/'
 })
